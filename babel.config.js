694b55de2e5ef9f902ffb8d9729228d7e4ede470
Sven Ingar Frantzen <sven.ingar@gmail.com>
2023-06-18 00:32:06 +0200

    Removed package_lock and sass dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,5 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
-  },
+  ]
 }
