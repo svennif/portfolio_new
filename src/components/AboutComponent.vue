8a58b21d04c2f4000b458910dba2359f49e0c09f
Sven Ingar Frantzen <sven.ingar@gmail.com>
2023-06-29 21:57:25 +0200

    .

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -1,6 +1,13 @@
 <template>
     <div id="about" class="mx-auto h-screen">
-            <h1 class="text-4xl text-highlight text-center mt-10 md:mt-24">About</h1>
+            <h1 class="text-4xl text-highlight text-center mt-10 md:mt-24">I'm Sven Ingar.</h1>
+            <div class="columns-2">
+                <div class="text-neutral-200"></div>
+                <div class="text-neutral-200">
+                    <p>I'm a frontend developer currently located in Copenhagen, Denmark.</p>
+                    <p></p>
+                </div>
+            </div>
     </div>
 </template>
 
