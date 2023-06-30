5751267024a82f83d4b3c347cbc1dfeaf46e34c1
Sven Ingar Frantzen <sven.ingar@gmail.com>
2023-06-30 16:09:58 +0200

    added a text about me

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -1,12 +1,13 @@
 <template>
     <div id="about" class="mx-auto h-screen">
-            <h1 class="text-4xl text-highlight text-center mt-10 md:mt-24">I'm Sven Ingar.</h1>
-            <div class="columns-2">
-                <div class="text-neutral-200"></div>
-                <div class="text-neutral-200">
-                    <p>I'm a frontend developer currently located in Copenhagen, Denmark.</p>
-                    <p></p>
-                </div>
+            <h1 class="text-4xl text-highlight text-center mt-10 md:mt-24">Who am I?</h1>
+            <div class="columns-2 flex justify-center">
+                <p class="w-1/2 p-10">
+                    I'm a proficient Frontend Developer with a diverse career journey. Starting with a decade-long tenure in retail management at Coop Obs Bryne, where I sharpened my customer service skills, I transitioned into the tech sector. My initial role was as an IT Consultant at Front Software, where I managed projects and worked extensively on digital control systems. Now, at Clerk.io, I serve as a Junior Integration Specialist, designing and implementing recommendation sliders for a variety of webshops using technologies like HTML, CSS, JavaScript, PHP, Smarty, and Liquid. Educationally, I hold a Bachelor's in New Media from the University of Bergen and a Higher Professional Degree in Frontend Development from Noroff. My belief in the importance of customer satisfaction reflects in my work, always ensuring a seamless user experience. In my free time, I stay updated with the latest in technology and UX design, and I enjoy outdoor activities for relaxation and rejuvenation. I am always eager for new challenges in the realm of frontend development.
+                </p>
+                <p class="text-neutral-200 w-1/2 p-10">
+                    I'm a proficient Frontend Developer with a diverse career journey. Starting with a decade-long tenure in retail management at Coop Obs Bryne, where I sharpened my customer service skills, I transitioned into the tech sector. My initial role was as an IT Consultant at Front Software, where I managed projects and worked extensively on digital control systems. Now, at Clerk.io, I serve as a Junior Integration Specialist, designing and implementing recommendation sliders for a variety of webshops using technologies like HTML, CSS, JavaScript, PHP, Smarty, and Liquid. Educationally, I hold a Bachelor's in New Media from the University of Bergen and a Higher Professional Degree in Frontend Development from Noroff. My belief in the importance of customer satisfaction reflects in my work, always ensuring a seamless user experience. In my free time, I stay updated with the latest in technology and UX design, and I enjoy outdoor activities for relaxation and rejuvenation. I am always eager for new challenges in the realm of frontend development.
+                </p>
             </div>
     </div>
 </template>
