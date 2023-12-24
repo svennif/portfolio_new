ea7978b0c202f80b010ac32077f8ae357afb3618
Sven Ingar Frantzen <sven.ingar@gmail.com>
2023-12-24 22:13:17 +0100

    Added Vercel SpeedInsights to the website

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
+<script setup>
+import { SpeedInsights } from "@vercel/speed-insights/vue"
+</script>
+
 <template>
     <Navbar />
     <router-view />
     <Footer />
+    <SpeedInsights />
 </template>
 
 <script>
-
 import Navbar from "@/components/common/NavbarComponent.vue";
 import Footer from "@/components/common/FooterComponent.vue";
 
